afae9b07bb7253d39284067b852a668a59c865ed
alireza momenpour <alirezamomenpour02@gmail.com>
2022-04-02 03:14:18 +0430

    animation two

--- app/src/main/java/com/digimoplus/myeducationcompose/uz/ButtonContent.kt
+++ app/src/main/java/com/digimoplus/myeducationcompose/uz/ButtonContent.kt
@@ -8,21 +8,25 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.digimoplus.myeducationcompose.uz.ui.purple500
+import kotlinx.coroutines.launch
 
 @Composable
 fun ButtonContent(
     textColor: Color,
-    state: MutableState<ButtonState>,
+    timeState: MutableState<Boolean>,
     iconAlpha: Float,
-    iconAnim :Dp
+    iconAnim: Dp
 ) {
-    if (state.value == ButtonState.IconAlphaOpen) {
+
+
+    if (timeState.value) {
         Row(verticalAlignment = Alignment.CenterVertically) {
             Column(Modifier.width(24.dp), horizontalAlignment = Alignment.CenterHorizontally) {
                 Icon(
@@ -47,5 +51,5 @@ fun ButtonContent(
             contentDescription = ""
         )
     }
-
 }
+
--- app/src/main/java/com/digimoplus/myeducationcompose/uz/FavButton.kt
+++ app/src/main/java/com/digimoplus/myeducationcompose/uz/FavButton.kt
@@ -23,6 +23,169 @@ import kotlinx.coroutines.launch
 fun FavButton() {
 
     val coroutineScope = rememberCoroutineScope()
+    val itemState = remember { mutableStateOf(ButtonState.IsOpen) }
+    val timeState = remember { mutableStateOf(true) }
+
+    val sizeWith by animateDpAsState(
+        targetValue = itemState.value.state[0].value as Dp,
+        animationSpec = tween(
+            delayMillis = 200,
+            durationMillis = 1500,
+            easing = FastOutLinearInEasing
+        )
+    )
+
+    val cornerRadius by animateDpAsState(
+        targetValue = itemState.value.state[1].value as Dp,
+        animationSpec = tween(
+            delayMillis = 200,
+            durationMillis = 1500,
+            easing = FastOutLinearInEasing
+        )
+    )
+
+    val textColor by animateColorAsState(
+        targetValue = itemState.value.state[2].value as Color,
+        animationSpec = tween(
+            delayMillis = 200,
+            durationMillis = 1500,
+            easing = FastOutLinearInEasing
+        )
+    )
+
+    val backColor by animateColorAsState(
+        targetValue = itemState.value.state[3].value as Color,
+        animationSpec = tween(
+            delayMillis = 200,
+            durationMillis = 1500,
+            easing = FastOutLinearInEasing
+        )
+    )
+
+    val iconAlpha by animateFloatAsState(
+        targetValue = itemState.value.state[4].value as Float,
+        animationSpec = tween(
+            delayMillis = 0,
+            durationMillis = 800,
+            easing = FastOutLinearInEasing
+        )
+    )
+
+    val myTransition = rememberInfiniteTransition()
+
+    val iconAnim by myTransition.animateValue(
+        initialValue = 24.dp,
+        targetValue = 12.dp,
+        typeConverter = Dp.VectorConverter,
+        animationSpec = infiniteRepeatable(
+            keyframes {
+                delayMillis = 1300
+                durationMillis = 1000
+                24.dp at 400
+                12.dp at 500
+                24.dp at 600
+                12.dp at 700
+            },
+            repeatMode = RepeatMode.Restart
+        )
+    )
+
+
+
+
+
+    Button(
+        colors = ButtonDefaults.buttonColors(backgroundColor = backColor),
+        border = BorderStroke(1.dp, Color.Blue),
+        shape = RoundedCornerShape(cornerRadius),
+        modifier = Modifier.size(sizeWith, 60.dp),
+        onClick = {
+            if (itemState.value == ButtonState.IsOpen) {
+                itemState.value = ButtonState.IsClose
+                coroutineScope.launch {
+                    delay(1400)
+                    timeState.value = false
+                }
+            } else {
+                itemState.value = ButtonState.IsOpen
+                coroutineScope.launch {
+                    delay(200)
+                    timeState.value = true
+                }
+            }
+        }
+    ) {
+        ButtonContent(
+            textColor,
+            timeState,
+            iconAlpha,
+            iconAnim
+        )
+    }
+
+}
+
+enum class ButtonValue(val value: Any) {
+    SizeOpen(300.dp),
+    SizeClose(60.dp),
+    CornerOpen(12.dp),
+    CornerClose(40.dp),
+    TextColorOpen(Color.Blue),
+    TextColorClose(Color.White),
+    BackColorOpen(Color.White),
+    BackColorClose(Color.Blue),
+    IconAlphaOpen(0f),
+    IconAlphaClose(1f)
+}
+
+enum class ButtonState(val state: List<ButtonValue>) {
+    IsOpen(
+        listOf(
+            ButtonValue.SizeOpen,
+            ButtonValue.CornerOpen,
+            ButtonValue.TextColorOpen,
+            ButtonValue.BackColorOpen,
+            ButtonValue.IconAlphaOpen,
+        )
+    ),
+    IsClose(
+        listOf(
+            ButtonValue.SizeClose,
+            ButtonValue.CornerClose,
+            ButtonValue.TextColorClose,
+            ButtonValue.BackColorClose,
+            ButtonValue.IconAlphaClose,
+        )
+    )
+}
+
+
+/*
+package com.digimoplus.myeducationcompose.uz
+
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Colors
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.digimoplus.myeducationcompose.uz.ui.purple500
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@Composable
+fun FavButton() {
+
+    val coroutineScope = rememberCoroutineScope()
     val sizeState = remember { mutableStateOf(ButtonState.SizeOpen) }
     val cornerState = remember { mutableStateOf(ButtonState.CornerOpen) }
     val textColorState = remember { mutableStateOf(ButtonState.TextColorOpen) }
@@ -95,7 +258,8 @@ fun FavButton() {
 
 
     //  val myTransition = rememberInfiniteTransition()
-    /* val borderWith by myTransition.animateColor(
+    */
+/* val borderWith by myTransition.animateColor(
          initialValue = MaterialTheme.colors.primary,
          targetValue = Color.Red,
          animationSpec = infiniteRepeatable(
@@ -106,7 +270,8 @@ fun FavButton() {
              ),
              repeatMode = RepeatMode.Reverse
          )
-     )*/
+     )*//*
+
 
 
     Button(
@@ -146,7 +311,7 @@ fun FavButton() {
 
 }
 
-enum class ButtonState(val value: Any) {
+enum class ButtonValue(val value: Any) {
     SizeOpen(300.dp),
     SizeClose(60.dp),
     CornerOpen(12.dp),
@@ -159,3 +324,33 @@ enum class ButtonState(val value: Any) {
     IconAlphaClose(1f)
 }
 
+enum class ButtonState(val value:List<ButtonValue>){
+    IsOpen(listOf(
+        ButtonValue.SizeOpen,
+        ButtonValue.CornerOpen,
+        ButtonValue.TextColorOpen,
+        ButtonValue.BackColorOpen,
+        ButtonValue.IconAlphaOpen,
+    )),
+    IsClose(listOf(
+        ButtonValue.SizeClose,
+        ButtonValue.CornerClose,
+        ButtonValue.TextColorClose,
+        ButtonValue.BackColorClose,
+        ButtonValue.IconAlphaClose,
+    ))
+}*/
+
+//  val myTransition = rememberInfiniteTransition()
+/* val borderWith by myTransition.animateColor(
+     initialValue = MaterialTheme.colors.primary,
+     targetValue = Color.Red,
+     animationSpec = infiniteRepeatable(
+         tween(
+             delayMillis = 1000,
+             durationMillis = 200,
+             easing = FastOutLinearInEasing
+         ),
+         repeatMode = RepeatMode.Reverse
+     )
+ )*/
